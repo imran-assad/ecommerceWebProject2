dc822f1ea4c7c24803113522829e6bc2e33419fb
Imran Asad <imranasad5252@gmail.com>
2024-09-02 09:39:10 +0500

    data fetching complete

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -1,16 +1,35 @@
-import React, { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
 import Title from "./Title";
+import ProductItem from "./ProductItem";
 
 const LatestCollection = () => {
-  const products = useContext(ShopContext);
+  const { products } = useContext(ShopContext);
+  const [latestProduct, setLatestProducts] = useState([]);
+
+  useEffect(() => {
+    setLatestProducts(products.slice(0, 10));
+  }, []);
   console.log(products);
+
   return (
     <>
       <div className="my-10">
         <div className="text-center py-8 text-3xl">
           <Title text1={"LATEST"} text2={"COLLECTION"} />
         </div>
+      </div>
+      {/* Rendering products */}
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+        {latestProduct.map((item, index) => (
+          <ProductItem
+            key={index}
+            id={item._image}
+            image={item.image}
+            name={item.name}
+            price={item.price}
+          />
+        ))}
       </div>
     </>
   );

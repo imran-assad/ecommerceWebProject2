289c9b1930838aedb6fab2b879580611eb2a1b09
Imran Asad <imranasad5252@gmail.com>
2024-09-03 23:58:59 +0500

    f

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,12 +1,22 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { products } from "../assets/frontend_assets/assets";
 
 export const ShopContext = createContext();
 const ShopContextProvider = (props) => {
   const currency = "$";
   const delivery_fee = 10;
+  const [search, setSearch] = useState("");
+  const [showSearch, setShowSearch] = useState(true);
 
-  const value = { products, currency, delivery_fee };
+  const value = {
+    products,
+    currency,
+    delivery_fee,
+    search,
+    setSearch,
+    showSearch,
+    setShowSearch,
+  };
   return (
     <ShopContext.Provider value={value}>{props.children}</ShopContext.Provider>
   );

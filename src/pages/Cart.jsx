8ff691ec27a7b1cdd978a676154caf83b2b8d1b7
Imran Asad <imranasad5252@gmail.com>
2024-09-05 03:32:14 +0500

    place oder section completed

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -4,7 +4,7 @@ import Title from "../components/Title";
 import { assets } from "../assets/frontend_assets/assets";
 import CartTotal from "../components/CartTotal";
 const Cart = () => {
-  const { products, currency, cartItems, updateQuantity } =
+  const { products, currency, cartItems, updateQuantity, navigate } =
     useContext(ShopContext);
 
   const [cartData, setCartData] = useState([]);
@@ -87,6 +87,14 @@ const Cart = () => {
         <div className="flex justify-end my-20">
           <div className="w-full sm:w-[450px]">
             <CartTotal />
+            <div className="w-full text-end">
+              <button
+                onClick={() => navigate("/place-order")}
+                className="bg-black text-white text-sm my-8 px-8 py-3"
+              >
+                PROCEED TO CHECKOUT
+              </button>
+            </div>
           </div>
         </div>
       </div>

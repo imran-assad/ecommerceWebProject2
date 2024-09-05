64aed25d55e0911b20fd6dbff0cba416203c92be
Imran Asad <imranasad5252@gmail.com>
2024-09-05 20:22:50 +0500

    errors removed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import { assets } from "../assets/frontend_assets/assets";
 const Hero = () => {
   return (
     <>
-      <div className="flex flex-col sm:flex-row border border-gray-400">
+      <div className="flex flex-col sm:flex-row border  border-gray-400">
         {/* Hero Left Side */}
         <div className="w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0">
           {/* BESTSELLERS PART */}
